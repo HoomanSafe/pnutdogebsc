51d8d9c8012e53a7ca9564e37927d134d2bfe086
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-26 21:53:39 +0800

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo11.png";
 
 const Hero = () => {
   return (
@@ -7,17 +7,16 @@ const Hero = () => {
       <div className="container flex flex-col justify-center items-center gap-6">
         <img src={logo} alt="" className="w-[35rem] logo-img" />
         <h1 className="text-6xl capitalize hero-header font-secondary">
-          <p>welcome</p> <p>to</p> <p>half</p> <p>bitcoin</p>
+          <p>welcome</p> <p>to</p> <p>PNUT</p> <p>DOGE</p>
         </h1>
         <p className="hero-para max-w-[600px]">
-          Once upon a time, Bitcoin was soaring high, reaching new heights every
-          day. It had just hit a new ATH of $69,000! All the degens were talking
-          about it. But something happenedßß
+          With Trump's support, PDOGE symbolizes the fearless pursuit of success and freedom in the world of decentralized finance. Whether you're a PNUT lover,
+          a DOGE fan, or just someone who believes in breaking the mold, PDOGE welcomes you to join this groundbreaking journey.
         </p>
 
         <h2 className="font-secondary text-4xl hero-contract"></h2>
         <h2 className="px-6 ca-div py-4 border border-black rounded-full">
-          0x00000000000000000000000000x....
+          0xFcb0e64435fEe443e6889eE075dADB102C1B61f1....
         </h2>
       </div>
     </section>
